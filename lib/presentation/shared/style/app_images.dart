f1e9f0b28a05e64b2c8fe43639aff604e4b978d1
Jhonathan <jhonathanqz011@gmail.com>
2021-12-13 14:44:32 -0300

    Configure new logo from project and splash screen

--- lib/presentation/shared/style/app_images.dart
+++ lib/presentation/shared/style/app_images.dart
@@ -1,4 +1,7 @@
 class AppImages {
   static String background = 'assets/background.jpg';
   static String logo = 'images/logo.png';
+  static String fundo1 = 'images/fundo1.jpg';
+  static String fundo2 = 'images/fundo2.jpg';
+  static String fundo3 = 'images/fundo3.jpg';
 }
